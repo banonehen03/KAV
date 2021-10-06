963213b39bd0f43c3e3bfa370ec2d4fd7ff1e0c2
banonehen03 <89727348+banonehen03@users.noreply.github.com>
2021-10-06 13:56:06 +0700

    Add files via upload

---
+++ modules/commands/teach.js
@@ -0,0 +1,28 @@
+/**
+* @author Quốc Anh
+* @warn Do not edit code or edit credits
+*/
+
+module.exports.config = {
+	name: "teach",
+	version: "2.0.3",
+	hasPermssion: 0,
+	credits: "Quốc Anh",
+	description: "Dạy bot (dùng cho lệnh sim)",
+	commandCategory: "general",
+	usages: "hello => goodbye",
+	cooldowns: 5,
+	dependencies: {
+		"axios": ""
+	}
+};
+
+module.exports.run = async function({ api, event, args }) {
+	const axios = global.nodemodule["axios"];
+	const res = await axios("https://meewmeew.info/simsimi/teach", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		data: { teach: args.join(" ") }
+	})
+	return api.sendMessage(`${res.data.success}`, event.threadID, event.messageID);
+}
