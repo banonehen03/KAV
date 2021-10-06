9143bd11003eccc1f50ffcdb95bdbeed129212e6
banonehen03 <89727348+banonehen03@users.noreply.github.com>
2021-10-06 14:04:19 +0700

    Add files via upload

---
+++ utils/log.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk');
+
+module.exports = (data, option) => {
+	switch (option) {
+		case "warn":
+			console.log(chalk.yellow('[ ❕ ] » ') + data);
+			break;
+		case "error":
+			console.log(chalk.red('[ ❕ ] » ') + data);
+			break;
+		default:
+			console.log(chalk.magenta(`${option} » `) + data);
+			break;
+	}
+}
+
+module.exports.loader = (data, option) => {
+	switch (option) {
+		case "warn":
+			console.log(chalk.yellow('[ Quốc Anh ] » ') + data);
+			break;
+		case "error":
+			console.log(chalk.red('[ Quốc Anh ] » ') + data);
+			break;
+		default:
+			console.log(chalk.green(`[ Quốc Anh ] » `) + data);
+			break;
+	}
+}
